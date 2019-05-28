d403c0f9348008733e4b71a3d4beb187e8ef8e0d
Federico Lupis <federico.lupis@hotmail.it>
2019-05-28 11:22:43 +0200

    first commit

---
+++ src/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "./Game.h"
+#include "./Constants.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  Game *game = new Game();
+
+  game ->Initialize(WINDOW_WIDTH,WINDOW_HEIGHT);
+
+  while (game->IsRunning()){
+    game -> ProcessInput();
+    game -> Update();
+    game -> Render();
+  }
+
+  game -> Destroy();
+
+
+  return 0;
+}
